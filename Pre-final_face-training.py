db907178d24187ce58977053a852c6587fb54111
Flawzik <79632599+Flawzik@users.noreply.github.com>
2022-11-20 20:35:38 +0300

    Update Pre-final_face-training.py

--- Pre-final_face-training.py
+++ Pre-final_face-training.py
@@ -1,54 +1,51 @@
-import pickle
 import cv2
 import os
 import numpy as np
 from PIL import Image
-BASE_DIR = os.path.dirname(os.path.abspath((__file__)))
-image_dir = os.path.join(BASE_DIR, 'images')
+import pickle
 
-face_cascade_db = cv2.CascadeClassifier(cv2.data.haarcascades+"haarcascade_frontalface_default.xml")
+BASE_DIR = os.path.dirname(os.path.abspath(__file__))
+image_dir = os.path.join(BASE_DIR, "images")
+
+face_cascade = cv2.CascadeClassifier('cascades/data/haarcascade_frontalface_alt2.xml')
 recognizer = cv2.face.LBPHFaceRecognizer_create()
+
 current_id = 0
-labels_ids = {}
+label_ids = {}
 y_labels = []
 x_train = []
+
 for root, dirs, files in os.walk(image_dir):
-    for file in files:
-        if file.endswith('png') or file.endswith("jpg"):
-            path = os.path.join(root, file)
-            label = os.path.basename(root).replace(' ', '-') #.lower# наш человек (папка)
-          #  print(label, path)
-            if label in labels_ids:
-                pass
-            else:
-                labels_ids[label] = current_id
-                current_id += 1
-            id_ = labels_ids[label]
-           # print(labels_ids)
-            y_labels.append(label) # какое то число
-            x_train.append(path)# в нампай массив
-            pil_image = Image.open(path).convert('L') # серый слой
-            size = (550, 550)
-            final_image = pil_image.resize(size, Image.ANTIALIAS)
-            image_array = np.array(final_image, "uint8")
-           # print(image_array)
-            faces = face_cascade_db.detectMultiScale(image_array)
-            for (x, y, w, h) in faces:
-                roi = image_array[y:y+h, x:x+w]
-                x_train.append(roi)
-                y_labels.append(id_)
-"""
-for image in os.listdir(BASE_DIR):
-  im = Image.open(BASE_DIR)
-  # If is png image
-  if im.format is 'PNG':
-    # and is not RGBA
-    if im.mode is not 'RGBA':
-      im.convert("RGBA").save(f"{BASE_DIR}2.png")
-"""
+	for file in files:
+		if file.endswith("png") or file.endswith("jpg") or file.endswith("jpeg"):
+			path = os.path.join(root, file)
+			label = os.path.basename(root).replace(" ", "-").lower()
+			#print(label, path)
+			if not label in label_ids:
+				label_ids[label] = current_id
+				current_id += 1
+			id_ = label_ids[label]
+			#print(label_ids)
+			#y_labels.append(label) # some number
+			#x_train.append(path) # verify this image, turn into a NUMPY arrray, GRAY
+			pil_image = Image.open(path).convert("L") # grayscale
+			size = (550, 550)
+			final_image = pil_image.resize(size, Image.ANTIALIAS)
+			image_array = np.array(final_image, "uint8")
+			#print(image_array)
+			faces = face_cascade.detectMultiScale(image_array, scaleFactor=1.5, minNeighbors=5)
+
+			for (x,y,w,h) in faces:
+				roi = image_array[y:y+h, x:x+w]
+				x_train.append(roi)
+				y_labels.append(id_)
+
+
 #print(y_labels)
 #print(x_train)
-with open('labels.pickle', 'wb') as f:
-    pickle.dump(labels_ids, f)
+
+with open("labels.pickle", 'wb') as f:
+	pickle.dump(label_ids, f)
+
 recognizer.train(x_train, np.array(y_labels))
-recognizer.save('trainner.yml')
+recognizer.save("train.yml")
